f8be13e89730d942511264ac245fd001420c3e1c
janickvw <janick.van.well@gmail.com>
2019-06-14 22:47:16 +1200

    adding functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,22 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void buttonPressed(bool userPicked) {
+    if (userPicked ==
+        quizBrain.getQuestionAnswer(quizBrain.getQuestionNumber())) {
+      setState(() {
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+      });
+    } else {
+      setState(() {
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+      });
+    }
+    quizBrain.nextQuestion();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(quizBrain.getQuestionNumber()),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +82,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                buttonPressed(true);
               },
             ),
           ),
@@ -80,11 +101,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                buttonPressed(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
